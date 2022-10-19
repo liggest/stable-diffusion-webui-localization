8c4abdadd66d29aaa15bc0dbb19944f4c7a533f8
liggest <liggest@sina.cn>
2022-10-19 09:35:45 +0900

    add comments
    might just be a temporary solution

--- collect_tags.py
+++ collect_tags.py
@@ -1,3 +1,8 @@
+"""
+    提供若干 tag-翻译 对照文件、分隔符，读取 tag 和翻译
+    参考生成的 danbooru-untranslated.csv ，将读到的翻译填充至 danbooru-zh.csv
+"""
+
 from pathlib import Path
 
 # dan=Path("./tags/danbooru.csv")
@@ -14,15 +19,23 @@ while path:=input("tag-翻译 对照文件路径：\n"):
     with path.open(encoding="utf-8") as f:
         for line in f:
             line=line.strip()
-            if not line:
+            if not line: # 忽略空行
                 continue
-            key,*val=line.split(sep=splitter,maxsplit=1)
-            if not val:
+            key,*val=line.split(sep=splitter)
+            if not val: # 忽略没有分隔符的行
                 continue
-            key=key.strip().lower().replace(" ","_")
-            val=(splitter or " ").join(val)
+            key=key.strip().lower().replace(" ","_") # A key => a_key
+
+            # # 如果要读取 danbooru-untranslated.csv 里面有翻译的项
+            # # 可以交换注释和未注释部分
+            # if len(val)<2: 
+            #     continue
+            # val=val[-1].strip('"') # 有多个分隔符的行，取最后一项
+            val=(splitter or " ").join(val).strip('"')
+
             if key in tags and tags[key]!=val:
                 print(f"重复的 tag {key}  翻译 {tags[key]} => {val}")
+                # 多次读到相同的 tag，后读到的翻译会覆盖先读到的
             tags[key]=val
             count+=1
     print(f"读取了 {count} 个 tag")
@@ -33,9 +46,15 @@ with dan.open(encoding="utf-8") as f:
     with new_dan.open("a",encoding="utf-8") as nf:
         count=0
         for line in f:
-            key,_=line.split(",",maxsplit=1)
+            # 同上
+            # key,*vals=line.split(",") # 读 dan 里面的 tag
+            key,_=line.split(",",maxsplit=1) # 读 dan 里面的 tag
+
             if key in tags:
-                nf.write(f'{line.strip()},"{tags[key]}"\n')
+                # 若 tag 有翻译，也在 dan 中
+                # 同上
+                # nf.write(f'{key},{val[0]},"{tags[key]}"\n') # tag,类型序号,"翻译"
+                nf.write(f'{line.strip()},"{tags[key]}"\n') # tag,类型序号,"翻译"
                 count+=1
 
 print(f"写入 {count} 个 tag 至 {new_dan}")
--- get_untranslated.py
+++ get_untranslated.py
@@ -1,3 +1,8 @@
+"""
+    对比 danbooru.csv 和 danbooru-zh.csv
+    找出目前尚未翻译的 tag，生成 danbooru-untranslated.csv
+"""
+
 from pathlib import Path
 
 dan=Path("./tags/danbooru.csv")
@@ -8,6 +13,7 @@ tags=set()
 
 with new_dan.open(encoding="utf-8") as f:
     tags=set( line.split(",",maxsplit=1)[0] for line in f )
+    # 读取已翻译的 tag
 
 total=0
 with dan.open(encoding="utf-8") as f:
@@ -16,8 +22,8 @@ with dan.open(encoding="utf-8") as f:
         for i,line in enumerate(f):
             key,_=line.split(",",maxsplit=1)
             if key not in tags:
-                uf.write(line)
+                uf.write(line) # 未翻译的 tag 写入 untrans_dan
                 count+=1
-        total=i+1
+        total=i+1 # 只是计数
 
 print(f"{total}条中，{len(tags)}条已有翻译，{count}条尚无翻译")
